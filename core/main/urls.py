a7b38aeea97909cd93324d7628f1c8e6ea9862e6
Freyze10 <bjabillanoza@gmail.com>
2026-07-11 09:04:11 +0800

    deleted the product

--- core/main/urls.py
+++ core/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.index, name='index'),
     path('other/', views.otherPage, name='other'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('cmf/records/', views.cmf_records, name='cmf_records'),
